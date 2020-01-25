2ddac7dafbd9deaa80d6c23b9032fd467bdc215b
Olyno <alexlew95@gmail.com>
2020-01-25 03:30:57 +0100

    [master] - [fix] - Fix new music player

--- src/components/SongPlayer.svelte
+++ src/components/SongPlayer.svelte
@@ -1,39 +1,32 @@
 <script>
 
-    import { onMount, onDestroy } from 'svelte';
-    import YouTubePlayer from 'youtube-player';
+    import { onMount, onDestroy, afterUpdate } from 'svelte';
 
     export let song;
     export let currentLyric;
 
-    let player;
+    let player,
+        playerLoaded = false,
+        playerId = song.youtubeId;
+        
     let currentTimeFunction,
         currentMilliseconds = 0,
         milliseconds = 0,
         seconds = 0,
         minutes = 0;
 
-    function injectBefore(scds, text) {
-        const startTime = new Number(Object.keys(song.time)[0].split('.')[0].split(':')[1]);
-        const time = `00:${scds < 10 ? '0' : ''}${startTime - scds}.00`;
-        song.time[time] = text;
-    }
-
-    onMount(async () => {
-        injectBefore(1, 'Go!');
-        injectBefore(2, 'Ready?');
-        player = await YouTubePlayer('player', {
-            width: '600',
-            height: '400',
-            videoId: song.youtubeId,
-            playerVars: {
-                iv_load_policy: 3
-            }
-        });
-        player.playVideo()
-            .then(video => {
+    $: playerConfig = {
+        width: '600',
+        height: '400',
+        videoId: song.youtubeId,
+        playerVars: {
+            iv_load_policy: 3
+        },
+        events: {
+            onReady: () => {
+                player.playVideo();
                 currentTimeFunction = setInterval(() => {
-                    currentMilliseconds = video.getCurrentTime();
+                    currentMilliseconds = player.getCurrentTime();
                     milliseconds = new Number(currentMilliseconds.toFixed(2).split('.')[1]);
                     seconds = Math.floor(currentMilliseconds % 60);
                     minutes = Math.floor(new Number(currentMilliseconds.toFixed(1).split('.')[0]) / 60);
@@ -44,7 +37,30 @@
                         currentLyric = song.time[`${minutes}:${seconds}.${milliseconds}`];
                     }
                 }, 10);
-            })
+                playerLoaded = true;
+            }
+        }
+    }
+
+    function injectBefore(scds, text) {
+        const startTime = new Number(Object.keys(song.time)[0].split('.')[0].split(':')[1]);
+        const time = `00:${scds < 10 ? '0' : ''}${startTime - scds}.00`;
+        song.time[time] = text;
+    }
+
+    onMount(async () => {
+        player = new YT.Player('player', playerConfig);
+    })
+
+    afterUpdate(async () => {
+        if (currentTimeFunction) clearInterval(currentTimeFunction);
+        injectBefore(1, 'Go!');
+        injectBefore(2, 'Ready?');
+        if (playerLoaded && playerId !== song.youtubeId) {
+            player.destroy();
+            player = new YT.Player('player', playerConfig);
+            playerId = song.youtubeId;
+        }
     })
 
     onDestroy(async () => {
